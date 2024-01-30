a7c4bb80fb7e7b07ef8e73edac87de9be203b4e9
Timo Baader <57661987+timoBaader@users.noreply.github.com>
2024-01-30 11:55:26 +0200

    fixing availability zones

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-north-1", "eu-west-1b", "eu-west-1c"]
+  azs             = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
