ad82c9499b117788920714c4780e942091e1f244
Timo Baader <57661987+timoBaader@users.noreply.github.com>
2024-01-30 13:10:01 +0200

    autoscaling

--- main.tf
+++ main.tf
@@ -33,17 +33,20 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
+  
+  name = "blog"
+  min_size = 1
+  max_size = 2
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  vpc_zone_indetifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns 
+  security_groups     = [module.blog_sg.security_group_id]
 
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "HelloWorld"
-  }
+  image_id                 = data.aws_ami.app_ami.id
+  instance_type       = var.instance_type
 }
 
 module "blog_alb" {
