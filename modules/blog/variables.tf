6a4b0b56c303e78f832595e326aa3f371da95fee
Timo Baader <57661987+timoBaader@users.noreply.github.com>
2024-02-01 08:57:31 +0200

    access key vars

--- modules/blog/variables.tf
+++ modules/blog/variables.tf
@@ -41,3 +41,11 @@ variable "asg_max_size" {
   default     = 0
 }
 
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS Access Key ID"
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS Secret Access Key"
+}
+
